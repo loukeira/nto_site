b43619ddf18fc30d6b6c1c0e5b90524770c8d991
loukeira <markimsaraiva@gmail.com>
2018-09-10 16:04:54 -0300

    ok

--- layouts/revo/layout.php
+++ layouts/revo/layout.php
@@ -371,6 +371,19 @@ var FB_ForceReload = 0;
 			</div>
 		</div>
 		
+		<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b95dacbf31d0f771d8496d7/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 		
 		<div id="content_copyright"></div>
